2b0c9c6e88fc0bbead386139c2b98437a54fac9d
Saksit <saksit.sabaiporn@gmail.com>
2026-01-29 17:30:17 +0700

    feat_login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       {/* เมนู */}
       <nav style={{ display: 'flex', gap: 12 }}>
         <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
       </nav>
 
       <hr />
@@ -16,6 +18,8 @@ function App() {
       <Routes>
         <Route path="*" element={<NotFound />} />
          <Route path="/" element={<Home />} />
+         <Route path="/login" element={<Login />} />
+
     </Routes>
     </>
   )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <>
+      <h1>GIT 101 Version 0.0.2</h1>
+      <p>ยินดีต้อนรับสู่หน้า Login</p>
+    </>
+  )
+}
+
+export default Login
