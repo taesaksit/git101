4bdbce073a0a5b2574307b989644f5f459448d9f
Saksit <saksit.sabaiporn@gmail.com>
2026-02-05 11:52:07 +0700

    feat: add squash page

---
+++ src/pages/Squash.jsx
@@ -0,0 +1,11 @@
+function Squash() {
+  return (
+    <>
+      <h1>GIT 101 Version 0.0.2</h1>
+      <p>ยินดีต้อนรับสู่หน้า Squash</p>
+      <p>Add Something</p>
+    </>
+  )
+}
+
+export default Squash
