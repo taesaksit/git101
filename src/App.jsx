0bf7ed62bafc1781ef550b0607b63adfe8b2e1c1
Saksit <saksit.sabaiporn@gmail.com>
2026-02-05 10:59:28 +0700

    learn git rebase

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Login from './pages/Login'
 import FeatA from './pages/FeatA'
+import FeatB from './pages/FeatB'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
         <Link to="/feat_a"></Link>
+        <Link to="/feat_b"></Link>
       </nav>
 
       <hr />
@@ -22,6 +24,7 @@ function App() {
          <Route path="/" element={<Home />} />
          <Route path="/login" element={<Login />} />
          <Route path="/feat_a" element={<FeatA />} />
+         <Route path="/feat_b" element={<FeatB />} />
 
     </Routes>
     </>
---
+++ src/pages/FeatB.jsx
@@ -0,0 +1,10 @@
+function FeatB() {
+  return (
+    <>
+      <h1>GIT 101 Version 0.0.2</h1>
+      <p>ยินดีต้อนรับสู่หน้า FeatB</p>
+    </>
+  )
+}
+
+export default FeatB
