923bcba1470d4f535dc142d4fc684da770ab3015
Saksit <saksit.sabaiporn@gmail.com>
2026-02-05 11:21:54 +0700

    learn git rebase v2

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import NotFound from './pages/NotFound'
 import Login from './pages/Login'
 import FeatA from './pages/FeatA'
 import FeatB from './pages/FeatB'
+import FeatC from './pages/FeatC'
 
 function App() {
   return (
     <>
       {/* เมนู */}
       <nav style={{ display: 'flex', gap: 12 }}>
-        <Link to="/">Home</Link>
         <Link to="/login">Login</Link>
-        <Link to="/feat_a"></Link>
-        <Link to="/feat_b"></Link>
+        <Link to="/">Home</Link>
+        <Link to="/feat_a">FeatA</Link>
+        <Link to="/feat_b">FeatB</Link>
+        <Link to="/feat_c">FeatC</Link>
       </nav>
 
       <hr />
@@ -25,6 +27,7 @@ function App() {
          <Route path="/login" element={<Login />} />
          <Route path="/feat_a" element={<FeatA />} />
          <Route path="/feat_b" element={<FeatB />} />
+         <Route path="/feat_c" element={<FeatC />} />
 
     </Routes>
     </>
---
+++ src/pages/FeatC.jsx
@@ -0,0 +1,10 @@
+function FeatC() {
+  return (
+    <>
+      <h1>GIT 101 Version 0.0.2</h1>
+      <p>ยินดีต้อนรับสู่หน้า FeatC</p>
+    </>
+  )
+}
+
+export default FeatC
